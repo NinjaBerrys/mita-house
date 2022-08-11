1de50f2c47c5197a9c2d3f78fa3f9c099fb25f52
brian <ninjaberrys5355@gmail.com>
2022-08-11 20:31:55 +1200

    completed comments for main file

--- schedule code.py
+++ schedule code.py
@@ -1,86 +1,93 @@
 import re  # imports re module
 from guizero import *  # imports guizero
 
-import_list = []
-schedule_list = []
-y = []
-x = 0
+schedule_list = []  # creates the list called schedule_list
+import_list = []  # creates the list called import_list
+y = []  # creates the list called y because I didn't know what to name it
 
 
-def set_activity():
-    activity = input_activity_box.value
-    return activity
+def set_activity():  # defines the function set_activity
+    activity = input_activity_box.value  # creates the variable activity called activity and sets its value to the
+    # value inside of input_activity_box
+    return activity  # returns the variable activity
 
 
-def insert_activity():
-    def save_schedule_list():
-        with open("load_file", "w") as save_file:
-            for task in schedule_list:
+def insert_activity():  # defines the function insert_activity
+    def save_schedule_list():  # defines the function save_schedule_list inside of insert_activity
+        with open("load_file", "w") as save_file:  # creates load_file
+            for task in schedule_list:  # for every task in schedule list write it inside of load_file
                 save_file.write("%s\n" % task)
-            print("done")
+            save_file.close()  # closes load_file
 
-    def reset():
-        schedule_list.remove(event)
-        activity_1_box.destroy()
-        print(schedule_list)
-        app.update()
+    def delete():  # defines the function delete
+        schedule_list.remove(event)  # removes the item from schedule_list list
+        activity_1_box.destroy()  # destroys the box in which all the activities are in
+        app.update()  # updates the app so that the change is visible
 
-    subject = set_activity()
-    time_values = duration_to_time()
-    activity = [subject]
-    activity.extend(time_values)
-    schedule_list.append(activity)
-    print(schedule_list)
-    # for activities in schedule_list:
-    #     hours = activities[1]
-    PushButton(stats_box, text="save", command=save_schedule_list)
-    for index, event in enumerate(schedule_list):
-        activity_1_box = Box(schedule_box, layout="grid", grid=[index, 0])
-        Text(activity_1_box, text=event[0], grid=[0, 0])
-        Text(activity_1_box, text="{}h:{}m".format(event[1], event[2]), grid=[0, 1])
-        Box(activity_1_box, grid=[0, 2])
-        PushButton(activity_1_box, text="delete", grid=[1, 2], command=reset)
+    subject = set_activity()  # creates the subject variable and makes it equal to the result of the set_activity
+    # function
+    time_values = duration_to_time()  # creates the time_values variable and makes it equal to the result of the
+    # duration_to_time function
+    activity = [subject]  # creates list called activity with subject inside
+    activity.extend(time_values)  # appends time values to the end of activity
+    schedule_list.append(activity)  # appends the activity list into the schedule_list list
+    PushButton(stats_box, text="save", command=save_schedule_list)  # creates button which saves schedule_list
+    for index, event in enumerate(schedule_list):  # enumerates the list (assigns an index to each of the items)
+        activity_1_box = Box(schedule_box, layout="grid", grid=[index, 0])  # creates a box inside the schedule_box
+        # box for the task to be put inside and places the box index away from the left edge of the schedule_box box
+        Text(activity_1_box, text=event[0], grid=[0, 0])  # shows the name of the activity
+        Text(activity_1_box, text="{}h:{}m".format(event[1], event[2]),
+             grid=[0, 1])  # shows the duration of the activity
+        PushButton(activity_1_box, text="delete", grid=[1, 2],
+                   command=delete)  # creates a button for the delete function
 
-        stats_text.value = "your schedule is {}h:{}m long with {} separate activities".format(event[1], event[2],
-                                                                                              len(schedule_list))
-        app.update()
-    return schedule_list
-
-
-def duration_to_time():
-    duration = duration_box.value.split(":")
-    duration_in_hours = re.sub("\D", "", duration[0])
-    duration_in_minutes = re.sub("\D", "", duration[1])
-    time_values = [duration_in_hours, duration_in_minutes]
-    return time_values
+        stats_text.value = "you have scheduled {} separate activities".format(len(schedule_list))  # shows the amount
+        # of activities the user has made
+        app.update()  # updates the app so that changes are visible
 
 
-def load_schedule_file():
-    def save_schedule_list():
-        with open("save_file", "w") as save_file:
-            for task in schedule_list:
+def duration_to_time():  # defines the duration_to_time function
+    duration = duration_box.value.split(":")  # calls the value of duration_box and splits it in half from the ':'
+    # into a list consisting of the hours and minutes in string representation
+    duration_in_hours = re.sub("\D", "", duration[0])  # removes all non number characters to the hour item
+    duration_in_minutes = re.sub("\D", "",
+                                 duration[1])  # removes all non number characters to the minute item
+    time_values = [duration_in_hours, duration_in_minutes]  # creates a list featuring duration_in_hours and
+    # duration_in_minutes
+    return time_values  # returns the time_values list
+
+
+def load_schedule_file():  # defines the load_schedule_file function
+    def save_schedule_list():  # defines the save_schedule_list function inside the load_schedule_file function
+        with open("load_file", "w") as save_file:  # creates load_file
+            for task in y:  # for every task in y write it inside of load_file
                 save_file.write("%s\n" % task)
-            print("done")
+            save_file.close()  # closes load_file
 
-    with open("load_file", "r") as load_file:
+    def delete():  # defines the function delete
+        schedule_list.remove(event)  # removes the item from schedule_list list
+        activity_1_box.destroy()  # destroys the box in which all the activities are in
+        app.update()  # updates the app so that the change is visible
+
+    with open("load_file", "r") as load_file:  # opens the load_file in read mode as load_file
         for line in load_file:
-            pursuit = line[:-1]
-            pursuit.replace('"', "")
-            list(pursuit)
-            import_list.append(pursuit)
+            pursuit = line[:-1]  # removes linebreak
+            import_list.append(pursuit)  # appends pursuit to import_list list
         for i in import_list:
-            y.append(eval(i))
-        print(y)
-    PushButton(stats_box, text="save", command=save_schedule_list)
-    for index, event in enumerate(y):
-        activity_1_box = Box(schedule_box, layout="grid", grid=[index, 0])
-        Text(activity_1_box, text=event[0], grid=[0, 0])
-        Text(activity_1_box, text="{}h:{}m".format(event[1], event[2]), grid=[0, 1])
-        Box(activity_1_box, grid=[0, 2])
+            y.append(eval(i))  # look inside test files/string to list test file.py for more information
+    PushButton(stats_box, text="save", command=save_schedule_list)  # creates button which saves y
+    for index, event in enumerate(y):  # enumerates the list (assigns an index to each of the items)
+        activity_1_box = Box(schedule_box, layout="grid", grid=[index, 0])  # creates a box inside the schedule_box
+        # box for the task to be put inside and places the box index away from the left edge of the schedule_box box
+        Text(activity_1_box, text=event[0], grid=[0, 0])  # shows the name of the activity
+        Text(activity_1_box, text="{}h:{}m".format(event[1], event[2]), grid=[0, 1])  # shows the duration of the
+        # activity
+        PushButton(activity_1_box, text="delete", grid=[1, 2], command=delete)  # creates a button for the delete
+        # function
 
-        stats_text.value = "your schedule is {}h:{}m long with {} separate activities".format(event[1], event[2],
-                                                                                              len(y))
-        app.update()
+        stats_text.value = "you have scheduled {} separate activities".format(len(y))  # shows the amount
+        # of activities the user has made
+        app.update()  # updates the app so that changes are visible
 
 
 app = App(bg="grey")
--- test files/time duration test file.py
+++ test files/time duration test file.py
@@ -3,8 +3,10 @@ import re
 duration_options = ["0h:15m", "0h:30m", "0h:45m", "1h:00m", "1h:15m", "1h:30m", "1h:45m", "2h:00m", "2h:15m",
                     "2h"":30m", "2h:45m", "3h:00m"]
 duration = duration_options[10]
+print(duration)
 print(duration.split(":"))
 duration = duration.split(":")
+print(duration)
 print(re.sub("\D", "", duration[0]))
 print(re.sub("\D", "", duration[1]))
 
